a0a80da60612e35c885b96166d6adf0d8863f519
Alex Fuller <fulleralex47@gmail.com>
2021-01-04 09:16:35 -0500

    Server side plugin validation that can be implimented in the web.config and global.asax. If the user gets around the front end there will still be a server check in place.

--- unobtrusive.aspx.cs
+++ unobtrusive.aspx.cs
@@ -11,6 +11,9 @@ namespace ASPNET45
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //Does additional validation on the server so if the client gets around the front end, the server still checks for it.
+            //The validation is done in the web.config and the jquery is in the gloabal asax so it minimizes clutter and repetative 
+            //plugins
             //Can set unobtrusive for a particular page
             //Page.UnobtrusiveValidationMode = UnobtrusiveValidationMode.WebForms;
         }
